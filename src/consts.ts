4c705ec4295e91a58ecac10fc04b295f350f193d
Pepe Zarate <pepzarate@gmail.com>
2026-03-22 22:11:02 -0600

    feat: add hero image to first article, fix SITE_TITLE in consts, fix hero image responsive layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ErgoFresco';
+export const SITE_DESCRIPTION = 'Stay cool. Work better. Ergonomics and cooling gear for remote workers in warm climates.';
