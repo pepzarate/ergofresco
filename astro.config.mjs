dd8f9aaf749ab99c45845b6c585959f382836ebb
Pepe Zarate <pepzarate@gmail.com>
2026-03-23 17:32:53 -0600

    chore: remove tailwind css, project uses custom CSS with CSS variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ergofresco.com',
   integrations: [mdx(), sitemap()],
 
   vite: {
-    plugins: [tailwindcss()],
   },
 });
